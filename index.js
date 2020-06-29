4bfc324b6723053bc70f0b016b3410d5eb489df2
munnaislam9925 <munnaislam9925@gmail.com>
2020-06-29 19:37:56 +0600

    first website

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".flagClose").addEventListener("click",function(){
+    document.querySelector(".flag").style.opacity = "0";
+})
+document.querySelector(".flagOpen").addEventListener("click",function(){
+    document.querySelector(".flag").style.opacity = "1";
+})
